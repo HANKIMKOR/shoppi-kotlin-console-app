7a0f502abbe8245f05e875ea5c904957715e531a
HANKIMKOR <95111895+HANKIMKOR@users.noreply.github.com>
2022-12-13 18:37:06 +0900

    실습4-1

---
+++ src/main/kotlin/screen/ShoppingCategory.kt
@@ -0,0 +1,49 @@
+package screen
+
+/*
+Step 1. 장바구니에 추가한 상품 관리
+Step 2. 사용자 입력값 요청 처리 공통화
+Step 3. 프로젝트 전역에서 참조하는 상수
+
+ */
+class ShoppingCategory {
+    private fun showCategories() {
+        /*
+        Step1. 상품 카테고리 표기
+        Step2. 사용자 입력 받기
+        Step3. 사용자가 기대하는 값을 입력하지 않는 경우에 대한 처리
+         */
+        val categories = arrayOf("패션", "전자기기", "반려동물용품")
+        for (category in categories) {
+            println(category)
+        }
+        println("=> 장바구니로 이동하시려면 #을 입력해주세요.")
+
+        var selectedCategory = readLine()
+        while (selectedCategory.isNullOrBlank()) {
+            println("값을 입력해주세요")
+            selectedCategory = readLine()
+        }
+        if (selectedCategory == "#") {
+            // TODO 1. 장바구니 이동
+            val shoppingCart = ShoppingCart()
+            shoppingCart.showCartItems()
+        } else {
+            if (categories.contains(selectedCategory)) {
+                // TODO 2. 카테코리 상품 목록 보여주기
+                val shoppingProductList = ShoppingProductList()
+                shoppingProductList.showProducts(selectedCategory)
+            } else {
+                // TODO 3. 카테고리 목록에 없는 값을 입력하는 경우
+                showErrorMessage(selectedCategory)
+            }
+
+        }
+    }
+
+    private fun showErrorMessage(selectedCategory: String?) {
+        println("[$selectedCategory] : 존재하지 않는 카테고리 입니다. 다시 입력해주세요.")
+        showCategories()
+    }
+
+}
---
+++ src/main/kotlin/screen/ShoppingProductList.kt
@@ -0,0 +1,66 @@
+package screen
+
+import data.CartItems
+import data.Product
+
+class ShoppingProductList {
+    private val products = arrayOf(
+        Product("패션", "겨울 패딩"),
+        Product("패션", "겨울 바지"),
+        Product("전자기기", "핸드폰"),
+        Product("전자기기", "블루투스 이어폰"),
+        Product("전자기기", "노트북"),
+        Product("반려동물용품", "건식사료"),
+        Product("반려동물용품", "습식사료"),
+        Product("반려동물용품", "치약"),
+        Product("반려동물용품", "간식")
+    )
+    private val categories: Map<String, List<Product>> = products.groupBy {  product ->
+        product.categoryLabel
+    }
+
+    fun showProducts(selectedCategory: String) {
+        val categoryProducts = categories[selectedCategory]
+        if (!categoryProducts.isNullOrEmpty()) {
+            println("""
+                ***=========================================***
+                선택하신 [$selectedCategory] 카테고리 상품입니다.
+                """.trimIndent())
+
+            categoryProducts.forEachIndexed { index, product ->
+                println("${index}, ${product.name}")
+            }
+            showCartOption(categoryProducts, selectedCategory)
+        } else {
+            showEmptyProductMessage(selectedCategory)
+        }
+    }
+
+    private fun showCartOption(categoryProduct: List<Product>, selectedCategory: String) {
+        println(
+            """
+                ***=====================================****
+                장바구니에 담을 상품 번호를 선택해주세요.
+            """.trimIndent()
+        )
+
+        val selectedIndex = readLine()?.toIntOrNull()!!
+        categoryProduct.getOrNull(selectedIndex)?.let { product ->
+            CartItems.addProduct(product)
+            println("=> 장바구니로 이동하시려면 #을, 계속 쇼핑하시려면 *을 입력해주세요.")
+            val answer = readLine()
+            if (answer == "#") {
+                val shoppingCart = ShoppingCart()
+                shoppingCart.showCartItems()
+            } else if (answer == "*") {
+                showProducts(selectedCategory)
+            } else {
+                // TODO 그 외 값을 입력한 경우에 대한 처리
+            }
+
+        }
+    }
+    private fun showEmptyProductMessage(selectedCategory: String) {
+        println("[$selectedCategory] 카테고리의 상품이 등록되기 전입니다.")
+    }
+}
